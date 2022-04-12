bbc414903bab939ef6153b5b43633edda01f4266
mari <desmedtmarie@outlook.com>
2022-04-12 11:24:36 +0200

    restart

--- app.py
+++ app.py
@@ -1,17 +1,3 @@
-from flask import Flask, render_template
 
 
 
-app = Flask(__name__)
-
-@app.route("/")
-def main():
-    return render_template('index.html')
-
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
-
-
-if __name__ == "__main__":
-    app.run()
