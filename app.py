0a3848e1347b54f7f3bf8f2e9740e4617c52c02b
mari <desmedtmarie@outlook.com>
2022-04-12 13:39:46 +0200

    part1 ok

--- app.py
+++ app.py
@@ -1,50 +1,60 @@
-from flask import Flask, render_template, request,json
+from flask import Flask, render_template, json, request
 from flaskext.mysql import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
 
-
-app = Flask(__name__)
 mysql = MySQL()
- 
- 
+app = Flask(__name__)
+app.debug = True
+
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'jay'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'jay'
+app.config['MYSQL_DATABASE_USER'] = 'marie'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'marikiki9283'
 app.config['MYSQL_DATABASE_DB'] = 'BucketList'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
 
-@app.route("/")
+@app.route('/')
 def main():
     return render_template('index.html')
 
+
 @app.route('/signup')
-def signup():
+def showSignUp():
     return render_template('signup.html')
 
-@app.route('/api/signUp',methods=['POST'])
-def signUp():
-    # read the posted values from the UI
-    _name = request.form['inputName']
-    _email = request.form['inputEmail']
-    _password = request.form['inputPassword']
 
+@app.route('/api/signup', methods=['POST'])
+def signUp():
     conn = mysql.connect()
     cursor = conn.cursor()
-    
-    _hashed_password = generate_password_hash(_password)
-    cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+    try:
+        _name = request.form['inputName']
+        _email = request.form['inputEmail']
+        _password = request.form['inputPassword']
 
-    data = cursor.fetchall()
- 
- 
-    if len(data) is 0:
-        conn.commit()
-        return json.dumps({'message':'User created successfully !'})
-    else:
-        return json.dumps({'error':str(data[0])})
+        # validate the received values
+        if _name and _email and _password:
 
+
+            #_hashed_password = generate_password_hash(_password, method='pbkdf2:sha256', salt_length=16)
+            
+            cursor.callproc('sp_createUser', (_name, _email, _password))
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return json.dumps({'message': 'User created successfully !'})
+            else:
+                return json.dumps({'error': str(data[0])})
+        else:
+            return json.dumps({'html': '<span>Enter the required fields</span>'})
+
+    except Exception as e:
+        return json.dumps({'error': str(e)})
+    finally:
+        cursor.close()
+        conn.close()
 
 
 if __name__ == "__main__":
